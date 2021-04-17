5492833cede9bf1c5a9d199a078d9d33f1909ad4
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2021-04-17 19:50:12 +0530

    Update common.php

--- Malhar/common.php
+++ Malhar/common.php
@@ -1,6 +1,10 @@
 <?php
+//For Development
+//$con= mysqli_connect("localhost","root","","malhar")
 
-$con= mysqli_connect("localhost","root","","malhar")
+$con= mysqli_connect("remotemysql.com","IUQ8DZ1ksO","3HFAXtlYBM","IUQ8DZ1ksO")
+
 or die(mysqli_error($con));
 
 ?>
+
